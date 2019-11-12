7773c1d3613daffb6dcf0992dab3941a130e6cbe
Eugene <github@it-efrem.com>
2019-11-12 22:46:03 +0300

    + Useful page

---
+++ pages/useful_for_developers/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function UsefulForDevelopers() {
+    return (
+        <>
+            <Head>
+                <title>Eugene Efremov - UsefulForDevelopers | it-efrem.com</title>
+            </Head>
+            <div>
+                UsefulForDevelopers
+            </div>
+        </>
+    )
+}
