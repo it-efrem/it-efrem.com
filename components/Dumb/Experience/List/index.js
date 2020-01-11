255ffd4a8cf20f834b4a2865cf2b1e754c57d948
Eugene <github@it-efrem.com>
2020-01-11 16:04:22 +0300

    fix List

--- components/Dumb/Experience/List/index.js
+++ components/Dumb/Experience/List/index.js
@@ -1,16 +1,18 @@
 export function List({data, preKey = 0}) {
     return (
         <ul>
-            {data.map((item, idx) => {
-                    const key = `${preKey}-${idx}`;
+            {
+                data.map((item, idx) => {
+                        const key = `${preKey}-${idx}`;
 
-                    if (typeof item === 'string') {
-                        return <li key={key}><span dangerouslySetInnerHTML={{__html: item}}/></li>
-                    } else {
-                        return <List data={item} preKey={idx}/>
+                        if (typeof item === 'string') {
+                            return <li key={key}><span dangerouslySetInnerHTML={{__html: item}}/></li>
+                        } else {
+                            return <List key={idx} preKey={idx} data={item}/>
+                        }
                     }
-                }
-            )}
+                )
+            }
         </ul>
     );
 }
