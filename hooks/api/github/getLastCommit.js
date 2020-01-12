226d18bd2dffe17937954d77123f404c7504774c
Eugene <github@it-efrem.com>
2020-01-12 04:31:20 +0300

    -+ refactor getLastCommit

--- hooks/api/github/getLastCommit.js
+++ hooks/api/github/getLastCommit.js
@@ -3,37 +3,35 @@ import axios from 'axios'
 import moment from 'moment'
 
 export function getLastCommit() {
-    const [commits, setCommits] = useState();
     const [lastCommit, setLastCommit] = useState('Loading...');
 
 
-    useEffect(() => {
-        axios.get('https://api.github.com/repos/Evgeny-Bukovski/it-efrem.com/commits')
-            .then(response => {
-                setCommits(response.data)
-            })
-            .catch(err => {
-                setLastCommit('GitHub server is not available');
-            })
+    useEffect(async () => {
+        try {
+            const response = await axios.get('https://api.github.com/repos/Evgeny-Bukovski/it-efrem.com/commits');
+            setLastCommit(
+                getLastCommitDate(response.data)
+            )
+        } catch {
+            setLastCommit('GitHub server is not available')
+        }
     }, []);
 
-    useEffect(() => {
+    function getLastCommitDate(commits) {
         if (commits) {
             try {
-                const _data = commits[0].commit.committer.date;
+                const {date} = commits[0].commit.committer;
 
-                if (_data) {
-                    setLastCommit(
-                        moment(_data, moment.HTML5_FMT.DATETIME_LOCAL_MS).format('LL')
-                    );
+                if (date) {
+                    return moment(date, moment.HTML5_FMT.DATETIME_LOCAL_MS).format('LL');
                 } else {
                     throw new Error();
                 }
             } catch (err) {
-                setLastCommit('GitHub API changed')
+                return 'GitHub API changed'
             }
         }
-    }, [commits]);
+    }
 
     return lastCommit
 }
