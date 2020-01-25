012b4133296d860c983a854e0a5af128b2e7eadf
Eugene <github@it-efrem.com>
2020-01-25 23:55:12 +0300

    +FunctionalComponent vs ClassComponent

---
+++ pages/examples/ReactFunctionVsClass/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+import React, {useState} from 'react';
+
+export default function ReactFunctionVsClass() {
+    return (
+        <>
+            <FunctionalComponent/>
+            <ClassComponent/>
+        </>
+    )
+}
+
+const newState = {b: 2};
+
+function FunctionalComponent() {
+    const [state, setState] = useState({a: 1});
+
+    // Displays a 'FunctionalComponent' 3 times
+    // state used in DOM
+    console.log('FunctionalComponent', state);
+
+    return (
+        <div>
+            <button onClick={() => setState(newState)}>Function Click</button>
+            <span>{JSON.stringify(state)}</span>
+        </div>
+    )
+}
+
+class ClassComponent extends React.Component {
+    state = {a: 1};
+
+    render() {
+        // Displays a 'ClassComponent' infinity times
+        // state NOT used in DOM
+        console.log('ClassComponent', this.state);
+
+        return (
+            <div>
+                <button onClick={() => this.setState(newState)}>Class Click</button>
+            </div>
+        )
+    }
+}
