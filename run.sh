f75b355ee4604c4825584d05f28aeafab5360ed0
Eugene <git@it-efrem.com>
2020-08-31 00:29:15 +0300

    +packages

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-sudo npm run clean &&
-sudo npm run build &&
-pm2 start "sudo npm run start:prod" --name "main" --log
+# sudo npm run clean &&
+# sudo npm install &&
+# sudo npm run build &&
+# pm2 start "sudo npm run start:prod" --name "main" --log
