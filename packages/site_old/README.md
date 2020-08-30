f73ad0350a348727318c8741693aef96150c4714
Eugene <git@it-efrem.com>
2020-08-31 00:29:15 +0300

    +packages

---
+++ packages/site_old/README.md
@@ -0,0 +1,9 @@
+#Project structure
+###pages/
+Pages are associated with a route based on their file name. One `.js` file - one page -> https://nextjs.org/docs/basic-features/pages
+
+###pages/_app.js
+See 'Custom App' -> https://nextjs.org/docs/advanced-features/custom-app
+
+###src/pages/
+See an interesting approach to building the structure of pages and components for Next.js -> https://github.com/zeit/next.js/issues/8617#issuecomment-570067815
---
+++ packages/site_old/src/containers/Header/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import React, {useEffect, useState} from 'react';
+import Link from 'next/link';
+import {NavLink} from '../../components/NavLink';
+import classNames from 'classnames';
+import CSS from './style.scss'
+
+import {menuItems} from "../../common/data/menuItems";
+import {detectScroll} from "../../hooks/detectScroll";
+
+export default function Header() {
+    const isScroll = detectScroll();
+
+    return (
+        <header className={classNames({
+            [CSS.header_scroll]: isScroll
+        })}>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href='/'>
+                            <a>
+                                <span>About me</span>
+                                &nbsp;
+                                <mark>👉</mark>
+                            </a>
+                        </Link>
+                    </li>
+                    {menuItems.map(mapMenu)}
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function mapMenu(item) {
+    return (
+        <li key={item.label}>
+            <NavLink {...item}/>
+        </li>
+    );
+}
