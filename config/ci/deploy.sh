b89ca3679e33a0d64525d98cbf1a020f23a7da98
Eugene <git@it-efrem.com>
2020-09-17 02:00:22 +0300

    -+deploy.sh

--- config/ci/deploy.sh
+++ config/ci/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -ex # e - exit on error, x - print command before execution
 
-git reset --hard HEAD
-git pull
 docker-compose down -v --remove-orphans --rmi local
 docker-compose up --build --force-recreate --no-deps -d -V --remove-orphans
