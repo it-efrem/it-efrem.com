8e70aae1a8acd0202ac44daa73c071dc5b262259
Eugene <git@it-efrem.com>
2020-09-17 21:14:55 +0300

    git_increment_tag debug

--- config/ci/git_increment_tag.sh
+++ config/ci/git_increment_tag.sh
@@ -6,11 +6,10 @@ echo "git_increment_tag"
 #CURRENT_BRANCH=$(git branch --show-current)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-echo "$CURRENT_BRANCH: $CURRENT_BRANCH"
-
 if [[ "$CURRENT_BRANCH" == "master" ]]; then
   CURRENT_TAG=$(git describe --abbrev=0 --tags)
-  VERSION_BITS="${CURRENT_TAG//./ }"
+  # shellcheck disable=SC2206
+  VERSION_BITS=(${CURRENT_TAG//./ })
 
   VNUM1=${VERSION_BITS[0]}
   VNUM2=${VERSION_BITS[1]}
@@ -24,9 +23,9 @@ if [[ "$CURRENT_BRANCH" == "master" ]]; then
     echo "Already a tag on this commit"
   else
     NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
-    git tag $NEW_TAG
-    echo "Tagged with $NEW_TAG"
-    git push --tags
+#    git tag $NEW_TAG
+#    echo "Tagged with $NEW_TAG"
+#    git push --tags
   fi
 else
   echo "Branch isn't master"
