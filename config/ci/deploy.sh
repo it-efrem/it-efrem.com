49c8dbdc5c483dd146011ccda3fc3bba9a7b7fe6
Eugene <git@it-efrem.com>
2020-09-17 15:08:54 +0300

    git_increment_tag

--- config/ci/deploy.sh
+++ config/ci/deploy.sh
@@ -3,3 +3,4 @@ set -ex # e - exit on error, x - print command before execution
 
 docker-compose down -v --remove-orphans --rmi local
 docker-compose up --build --force-recreate --no-deps -d -V --remove-orphans
+sh ./git_increment_tag.sh
